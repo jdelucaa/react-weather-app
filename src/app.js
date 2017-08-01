e26008bcf71dd907efb7155ce198d679d0c92202
joana.deluca <joana.deluca@senior.com.br>
2017-08-01 12:20:55 -0300

    Getting something on a page.

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import './styles.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = document.getElementById('app-container');
+
+ReactDOM.render(<p>Hello World!</p>, root);
