f0e1505b2e474a8ca13278c4210d1dc1c355b544
joana.deluca <joana.deluca@senior.com.br>
2017-08-03 13:26:02 -0300

    Parent-Child Component Communication

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { get } from 'axios';
 import ZipForm from './ZipForm';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      zipcode: '',
+      city: {},
+      dates: [],
+      selectedDate: null
+    };
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit(zipcode) {
+    get(`http://localhost:3000/weather/${zipcode}`)
+      .then(({ data }) => {
+        const { city, list: dates } = data;
+
+        this.setState({ zipcode, city, dates, selectedDate: null });
+      });
+  }
+
   render() {
     return <div className="app">
-      <ZipForm />
+      <ZipForm onSubmit={this.onFormSubmit} />
     </div>;
   }
 }
--- src/components/ZipForm.js
+++ src/components/ZipForm.js
@@ -10,6 +10,17 @@ class ZipForm extends React.Component {
     };
 
     this.inputUpdated = this.inputUpdated.bind(this);
+    this.submitZipCode = this.submitZipCode.bind(this);
+  }
+
+  submitZipCode(e) {
+    e.preventDefault();
+
+    const { zipcode } = this.state;
+    const { onSubmit } = this.props;
+
+    onSubmit(zipcode);
+    this.setState({ zipcode: '' });
   }
 
   inputUpdated(e) {
@@ -21,14 +32,14 @@ class ZipForm extends React.Component {
   render() {
     return (
       <div className="zip-form">
-        <form>
+        <form onSubmit={this.submitZipCode}>
           <label htmlFor="zipcode">Zip Code</label>
           <input
             className="form-control"
             type="input"
             name="zipcode"
-            value={this.state.zipcode} 
-            onInput={this.inputUpdated}/>
+            value={this.state.zipcode}
+            onInput={this.inputUpdated} />
           <button type="submit" className='btn btn-success'>Get the forecast!!</button>
         </form>
       </div>
