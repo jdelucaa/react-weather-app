a6bad8e7c8da5e01d4a3f43fb469dbac058fc438
joana.deluca <joana.deluca@senior.com.br>
2017-08-21 12:33:47 -0300

    Lifecycle Methods.

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
+      weatherData: [],
       zipcode: '',
       city: {},
       dates: [],
@@ -19,13 +20,20 @@ class App extends React.Component {
     this.onDayClicked = this.onDayClicked.bind(this);
   }
 
-  onFormSubmit(zipcode) {
-    get(`http://localhost:3000/weather/${zipcode}`)
-      .then(({ data }) => {
-        const { city, list: dates } = data;
-
-        this.setState({ zipcode, city, dates, selectedDate: null });
+  componentDidMount() {
+    get(`http://localhost:3000/weather`)
+      .then(({ data: weatherData }) => {
+        this.setState({ weatherData });
       });
+  }
+
+  onFormSubmit(zipcode) {
+    const zip = zipcode * 1;
+    const { weatherData } = this.state;
+    const data = weatherData.find(w => w.id === zip);
+    const { city, list: dates } = data;
+
+    this.setState({ zip, city, dates, selectedDate: null });
   }
 
   onDayClicked(dayIndex) {
@@ -33,10 +41,11 @@ class App extends React.Component {
   }
 
   render() {
-    const { city, dates, selectedDate } = this.state;
+    const { weatherData, city, dates, selectedDate } = this.state;
+    const zips = weatherData.map(w => w.id);
 
     return <div className="app">
-      <ZipForm onSubmit={this.onFormSubmit} />
+      <ZipForm zips={zips} onSubmit={this.onFormSubmit} />
       <WeatherList days={dates} onDayClicked={this.onDayClicked} />
       {/* javascript returns the last thing that was true */}
       {selectedDate !== null && <CurrentDay city={city} day={dates[selectedDate]} />}
